90f356dc2fd1891bf29b5eccae9614c49d4edeff
Varshil-B-Shah <varshil4247shah@gmail.com>
2024-10-19 17:04:24 +0530

    Pushing code

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const Home = () => {
   return (
     <div className="text-purple-600 text-xl">
-      Hello
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
